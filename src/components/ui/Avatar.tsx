ae22e93c3bf817d29fae3c8204e5d6861df517eb
vishwasraju <154666189+vishwasraju@users.noreply.github.com>
2025-06-07 03:24:39 +0530

    Updated Avatar.tsx

--- src/components/ui/Avatar.tsx
+++ src/components/ui/Avatar.tsx
@@ -70,8 +70,8 @@ export default function Avatar({
       
       {verified && (
         <div className="absolute -bottom-1 -right-1 w-5 h-5 bg-purple-600 rounded-full flex items-center justify-center ring-2 ring-white">
-          <svg className="w-3 h-3 text-white" fill="currentColor" viewBox="0 0 20 20">
-            <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+          <svg className="w-3 h-3 text-white\" fill="currentColor\" viewBox="0 0 20 20">
+            <path fillRule="evenodd\" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z\" clipRule="evenodd" />
           </svg>
         </div>
       )}
